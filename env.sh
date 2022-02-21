dd698bb9a093d9bdb3f9f30251dd46d3d9d821c1
Rareș Nistor (Razz) <me@thegreatrazz.nz>
2022-02-21 23:04:08 +1300

    Change the Metlink GTFS link

--- env.sh
+++ env.sh
@@ -3,5 +3,5 @@
 export GITHUB_TOKEN=$GH_TOKEN
 export GIT_REPO="https://github.com/thegreatrazz/metlink-gtfs.git"
 export GIT_CLONE=metlink-gtfs
-export GTFS_SOURCE="https://old.metlink.org.nz/assets/Google_Transit/google-transit.zip"
+export GTFS_SOURCE="https://static.opendata.metlink.org.nz/v1/gtfs/full.zip"
 export GTFS_ZIP_NAME="metlink-gtfs-$RANDOM.zip"
